f3300ce7137b629ba7ab35c324017dca9e58721b
dodotronix <dodotronix@gmail.com>
2023-01-24 20:07:45 +0100

    added TODOs

--- lua/neoverilog/init.lua
+++ lua/neoverilog/init.lua
@@ -5,6 +5,8 @@
 -- TODO rewrite the code to be better organized
 -- TODO check for the library definitions
 -- TODO add configuration file to set: indent, extra libraries ...
+-- TODO notifie the user, that not all stars were unfolded
+-- TODO attache the fold and unfold functions to key shortcuts
 
 local api = vim.api
 local command = api.nvim_create_user_command
