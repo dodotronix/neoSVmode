1758887d98a161f4bad1338ddc9f925007459c2f
dodotronix <dodotronix@gmail.com>
2023-03-05 12:02:48 +0100

    finding macros in the module ready

--- lua/neoverilog/parser/module.lua
+++ lua/neoverilog/parser/module.lua
@@ -12,7 +12,8 @@ function M:new(module_tree, str_content)
               }
     setmetatable(d, self)
     self.__index = self
-    d:get_instantiations()
+    -- d:get_instantiations()
+    d:get_macros()
     -- d:get_raw_module()
     return d
 end
@@ -26,7 +27,6 @@ function M.from_str_content(str_content)
     return nil
 end
 
-
 function M:get_instantiations()
     local  instance_query = vim.treesitter.parse_query(
     "verilog", [[((module_or_generate_item) @t 
@@ -35,6 +35,35 @@ function M:get_instantiations()
         local node_text = ts_query.get_node_text(n, self.str_content, false)
         local i = Instance.from_str_content(node_text)
         table.insert(self.instances, i)
+    end
+end
+
+function M:get_macros()
+    local root = self.module_tree:root()
+    local module_ts_type = root:child():type()
+    local  instance_query = vim.treesitter.parse_query(
+    "verilog", [[
+    ((comment) @macro (#match? @macro "\\/\\*[A-Z]+\\*\\/"))
+    ((comment) @end (#match? @end "// End of automatics"))]])
+    for i, n in instance_query:iter_captures(root, self.str_content) do
+        local node_type = n:parent():type()
+        if node_type == module_ts_type then
+            local group = instance_query.captures[i]
+            local range = { n:range() }
+            if group == "macro" then
+                local txt = ts_query.get_node_text(n, self.str_content)
+                local name = string.gsub(txt, "/%*(%u+)%*/", "%1")
+                table.insert(self.macros, 1, {})
+                self.macros[1] = {name = name,
+                start_line = range[1],
+                stop_line = range[3]}
+            else
+                -- replace the end line number if the block
+                -- of variables is closed with the "// end 
+                -- of automatics" sentence
+                self.macros[1].stop_line = range[3]
+            end
+        end
     end
 end
 
