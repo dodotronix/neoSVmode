395c940cfe61e27997d49500dcb9bce8dea21c67
dodotronix <dodotronix@gmail.com>
2023-03-08 23:38:23 +0100

    created param and port parser using treesitter

--- lua/neoverilog/parser/instance.lua
+++ lua/neoverilog/parser/instance.lua
@@ -1,70 +1,91 @@
 local LanguageTree = require('vim.treesitter.languagetree')
+local ts_query = vim.treesitter.query
 
 I = {}
 
-function I:new(instance_tree, str_content, wrapped_content)
+function I:new(instance_tree, str_content, name)
     local d = {instance_tree = instance_tree,
                str_content = str_content,
-               name = "",
+               name = name,
                autoparam = false,
                autoinst = false,
-               assignments = {},
+               param_assignments = {},
+               port_assignments = {},
                prams_all = {},
                vars_all = {}
               }
     setmetatable(d, self)
     self.__index = self
-    d:get_ports()
+    d:get_parameters()
+    -- d:get_ports()
     -- d:get_raw_instance()
     return d
 end
 
 function I.from_str_content(str_content)
-    -- this is very important, if the parsed instance is placed inside a fake 
-    -- module, we can use the treesitter to parse the vars and params, because
-    -- the sexp names are then consistent 
-    local w_content = string.format("module w;\n%s\nendmodule", str_content)
+
+    local name = string.match(str_content, "[%w_]+")
+    -- IMPORTANT there is an issue with treesitter that it doesn't recognize 
+    -- module without the parameter brackets #(), therefore we have to inject
+    -- it to instances which don't have that
+    local ext_content = string.gsub(str_content, "([%w_]+)%s+([%w_]+.*)", "%1 #() %2")
+
+    -- this is very important, if the parsed instance is placed inside a dummy 
+    -- module, we can use the treesitter to parse the vars and identifiers 
+    local w_content = string.format("module w;\n%s\nendmodule", ext_content)
+
     local trees = LanguageTree.new(w_content, 'verilog', {})
     trees = trees:parse()
     if #trees > 0 then
-            return I:new(trees[1],  w_content)
+            return I:new(trees[1],  w_content, name)
     end
     return nil
 end
 
--- deprecated function - can't match interfaces
---[[ function I:parse_instance()
-    self.name = string.match(self.str_content, "([%w_]+)")
-    -- get brackets with prameters and ports
-    for i in  string.gmatch(self.str_content, "([^%.]%b())") do
-        local map = {}
-        for n in string.gmatch(i, "([%./][%w_%(%)%*/]+)[,%)]") do
-            print(n)
-            table.insert(map, 1, {})
-            map[1] = {
-                name = string.match(n, "%.?([/%*%w_]+)"),
-                value = string.match(n, "%((.-)%)")}
-            -- check if the name is a macro
-            if string.match(map[1].name, "/%*%u+%*/") then
-                self.auto = true
-            end
-        end
-        table.insert(self.assignments, 1, {})
-        self.assignments[1] = map
-    end
-end ]]
-
-function I.wrap_instance(content)
-end
-
 function I:get_ports()
-    print(self.str_content)
+    local root = self.instance_tree:root()
+    local ports_query = vim.treesitter.parse_query(
+    "verilog", [[(named_port_connection  
+                    (port_identifier) @id 
+                    (expression) @value)@line
+                ]])
+    for i, n in ports_query:iter_captures(root, self.str_content) do
+        local txt = ts_query.get_node_text(n, self.str_content)
+        local group = ports_query.captures[i]
+        if group == "line" then
+            table.insert(self.port_assignments, 1, {})
+        else
+            self.port_assignments[1][group] = txt
+        end
+    end
+    P(self.port_assignments)
 end
 
 function I:get_parameters()
+    local root = self.instance_tree:root()
+    local param_query = vim.treesitter.parse_query("verilog",
+    [[ (named_parameter_assignment  
+        (parameter_identifier) @id 
+        (param_expression) @value)@line  
+    ]])
+    for i, n in param_query:iter_captures(root, self.str_content) do
+        local txt = ts_query.get_node_text(n, self.str_content)
+        local group = param_query.captures[i]
+        if group == "line" then
+            table.insert(self.param_assignments, 1, {})
+        else
+            self.param_assignments[1][group] = txt
+        end
+    end
+    P(self.param_assignments)
 end
 
-function I:get_list_of_variables()
+function I:get_macros()
+
+end
+
+function I:get_formated_variables()
+
 end
 
 function I:get_portmap_from_definition()
