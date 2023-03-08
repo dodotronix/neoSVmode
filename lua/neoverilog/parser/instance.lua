ead8c4afe4f1b29638f3663955dd78e013e59579
dodotronix <dodotronix@gmail.com>
2023-03-08 09:02:50 +0100

    parsed instance wrapped into modul scope to be able to use treesitter

--- lua/neoverilog/parser/instance.lua
+++ lua/neoverilog/parser/instance.lua
@@ -2,37 +2,44 @@ local LanguageTree = require('vim.treesitter.languagetree')
 
 I = {}
 
-function I:new(instance_tree, str_content)
+function I:new(instance_tree, str_content, wrapped_content)
     local d = {instance_tree = instance_tree,
                str_content = str_content,
                name = "",
-               auto = false,
+               autoparam = false,
+               autoinst = false,
                assignments = {},
                prams_all = {},
                vars_all = {}
               }
     setmetatable(d, self)
     self.__index = self
-    d:parse_instance()
+    d:get_ports()
     -- d:get_raw_instance()
     return d
 end
 
 function I.from_str_content(str_content)
-    local trees = LanguageTree.new(str_content, 'verilog', {})
+    -- this is very important, if the parsed instance is placed inside a fake 
+    -- module, we can use the treesitter to parse the vars and params, because
+    -- the sexp names are then consistent 
+    local w_content = string.format("module w;\n%s\nendmodule", str_content)
+    local trees = LanguageTree.new(w_content, 'verilog', {})
     trees = trees:parse()
     if #trees > 0 then
-            return I:new(trees[1], str_content)
+            return I:new(trees[1],  w_content)
     end
     return nil
 end
 
-function I:parse_instance()
+-- deprecated function - can't match interfaces
+--[[ function I:parse_instance()
     self.name = string.match(self.str_content, "([%w_]+)")
     -- get brackets with prameters and ports
     for i in  string.gmatch(self.str_content, "([^%.]%b())") do
         local map = {}
         for n in string.gmatch(i, "([%./][%w_%(%)%*/]+)[,%)]") do
+            print(n)
             table.insert(map, 1, {})
             map[1] = {
                 name = string.match(n, "%.?([/%*%w_]+)"),
@@ -45,10 +52,19 @@ function I:parse_instance()
         table.insert(self.assignments, 1, {})
         self.assignments[1] = map
     end
+end ]]
+
+function I.wrap_instance(content)
+end
+
+function I:get_ports()
+    print(self.str_content)
+end
+
+function I:get_parameters()
 end
 
 function I:get_list_of_variables()
-
 end
 
 function I:get_portmap_from_definition()
