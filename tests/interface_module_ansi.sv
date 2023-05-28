4efdac48d876ede12f43bc3d6892a7c70d9034c9
dodotronix <dodotronix@gmail.com>
2023-05-28 14:30:00 +0200

    added the datatypes in the module with interfaces

--- tests/interface_module_ansi.sv
+++ tests/interface_module_ansi.sv
@@ -3,11 +3,12 @@ module interface_module_ansi (
     t_clock.consumer Clk_x,
     t_bus.master bus_x,
     iface.general general_x,
-    t_control.consumer ctrl_x, 
-    input c,
-    output d
+    t_control.consumer ctrl_x,
+    input logic c,
+    output logic d,
+    output wire ha
     );
 
-    // bodu of the interface_module_ansi 
+    // bodu of the interface_module_ansi
 
 endmodule
